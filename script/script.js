ee66d46439fa64419fd3083c1d3f37eded746dd7
Nav <nav123@gmail.com>
2023-03-20 09:25:30 -0700

    uses let or const to define a function

--- script/script.js
+++ script/script.js
@@ -1,5 +1,13 @@
-function greet(user){
-    console.log(`Hello ${user}`)
+// function greet(user){
+//     console.log(`Hello ${user}`)
+// }
+
+// greet("nav")
+
+
+let greet = function(user) {
+             console.log(`hello ${user}`)
+
 }
 
 greet("nav")
